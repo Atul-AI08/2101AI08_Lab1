7fc419e8c5787b24d5252efab3947a98d569af3c
Atul-AI08 <110095839+Atul-AI08@users.noreply.github.com>
2022-07-27 19:54:10 +0530

    added goto to restart calculator

--- calculator.c
+++ calculator.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 void main(){
     char a;
+    begin:
     printf("Enter the operation to be performed:\n'+' for addition\n'-' for subtraction\n'*' for multiplication \n'/' for divison\nInput: ");
-    scanf("%c",&a);
+    scanf("%1s",&a);
     int b,c;
     if (a=='+'){
         printf("Enter the first number: ");
@@ -10,6 +11,12 @@ void main(){
         printf("Enter the second number: ");
         scanf("%d",&c);
         printf("The sum of the numbers %d and %d is %d",b,c,b+c);
+        printf("Want to use the calculator again? y/n: ");
+        char d;
+        scanf("%1s",&d);
+        if (d=='y'){
+            goto begin;
+        }
     }
     else if (a=='-'){
         printf("Enter the first number: ");
@@ -17,6 +24,12 @@ void main(){
         printf("Enter the second number: ");
         scanf("%d",&c);
         printf("The difference of the numbers %d and %d is %d",b,c,b-c);
+        printf("Want to use the calculator again? y/n: ");
+        char d;
+        scanf("%1s",&d);
+        if (d=='y'){
+            goto begin;
+        }
     }
     else if (a=='*'){
         printf("Enter the first number: ");
@@ -24,6 +37,12 @@ void main(){
         printf("Enter the second number: ");
         scanf("%d",&c);
         printf("The product of the numbers %d and %d is %d",b,c,b*c);
+        printf("Want to use the calculator again? y/n: ");
+        char d;
+        scanf("%1s",&d);
+        if (d=='y'){
+            goto begin;
+        }
     }
     else if (a=='/'){
         printf("Enter the first number: ");
@@ -31,5 +50,15 @@ void main(){
         printf("Enter the second number: ");
         scanf("%d",&c);
         printf("The divison of the numbers %d and %d is %d",b,c,b/c);
+        printf("Want to use the calculator again? y/n: ");
+        char d;
+        scanf("%1s",&d);
+        if (d=='y'){
+            goto begin;
+        }
+    }
+    else{
+        printf("\nEnter the correct operation!!\n");
+        goto begin;
     }
 }
